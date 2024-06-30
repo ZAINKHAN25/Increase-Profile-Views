468403b5192962c501e86dd0e1cbb5b2f9425917
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-06-30 23:27:56 +0500

    Add event listener to name and quantity

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ const name = document.querySelector(".User-Name-Input");
 const quantity = document.querySelector(".Quantity-Increase-Input");
 const submitBtn = document.querySelector(".Submit-Btn");
 
-submitBtn.addEventListener("click", () => {
-
-    for (let i = 0; i <= quantity.value; i++) {
-        window.open(`https://github.com/${name.value}`);    
+function increaseView() {
+    for (let i = 1; i <= quantity.value; i++) {
+        window.open(`https://github.com/${name.value}`);
     };
+};
 
-})  
+
+// Event Listeners 
+
+submitBtn.addEventListener("click", increaseView);
+
+name.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        increaseView();
+    };
+});
+
+quantity.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        increaseView();
+    };
+});
